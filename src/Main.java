5a6e6fe8ffca1cbe15b0a2f8930ec3346c4a57b2
OTCLICK <ivan.budko.2004@mail.ru>
2024-11-20 22:03:28 +0300

    Add 2

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ class Main{
     public static void main(String[] args) {
         System.out.print("Это добавлено в ветке main");
         System.out.println("Добавление 1");
+        System.out.println("Добавление 2");
     }
 }
